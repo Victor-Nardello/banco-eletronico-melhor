0c2504aa545f237c6966795c4d09eccdf43398ff
Victor Nardello <victornardello@olosko.com>
2020-05-22 10:46:40 -0300

    Criando service e o implementation

--- caixa-eletronico/caixa-eletronico/caixa-eletronico/src/main/java/br/com/ciandt/caixaeletronico/dataprovider/implementation/TaxaJurosImplementation.java
+++ caixa-eletronico/caixa-eletronico/caixa-eletronico/src/main/java/br/com/ciandt/caixaeletronico/dataprovider/implementation/TaxaJurosImplementation.java
@@ -2,18 +2,89 @@ package br.com.ciandt.caixaeletronico.dataprovider.implementation;
 
 import java.math.BigDecimal;
 
-import br.com.ciandt.caixaeletronico.usecase.domain.TaxaJurosCCDomain;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import br.com.ciandt.caixaeletronico.dataprovider.entity.TaxaJurosEntity;
+import br.com.ciandt.caixaeletronico.dataprovider.mapper.TaxaDataproviderDomainMapper;
+import br.com.ciandt.caixaeletronico.usecase.domain.TaxaJurosCCDomain;
+import br.com.ciandt.caixaeletronico.usecase.domain.TaxaJurosCPDomain;
+
+@Component
 public class TaxaJurosImplementation {
+
+	@Autowired
+	TaxaDataproviderDomainMapper taxaDataproviderDomainMapper;
+
+	public TaxaJurosCCDomain getTaxaJurosCC15() {
+
+		TaxaJurosEntity taxaJurosEntity = new TaxaJurosEntity();
+		taxaJurosEntity.setAgencia("1234");
+		taxaJurosEntity.setConta("CC");
+		taxaJurosEntity.setSaldo(new BigDecimal(Math.floor(Math.random() * 99 - 1) + -999));
+		taxaJurosEntity.setLimite(new BigDecimal(6000));
+		taxaJurosEntity.setTaxaJuros(new BigDecimal(15));
+
+		return taxaDataproviderDomainMapper.getTaxaJurosCC(taxaJurosEntity);
+	}
 	
-	public TaxaJurosCCDomain getTaxaJurosCC() {
-		
-		TaxaJurosCCDomain taxaJurosCCDomain = new TaxaJurosCCDomain();
-		taxaJurosCCDomain.setAgencia("1234");
-		taxaJurosCCDomain.setConta("CC");
-		taxaJurosCCDomain.setSaldo(new BigDecimal(Math.floor(Math.random()*99 - 1) + -999));
-		taxaJurosCCDomain.setLimite(new BigDecimal(6000));
-		
-		return taxaJurosCCDomain;
+	public TaxaJurosCCDomain getTaxaJurosCC10() {
+
+		TaxaJurosEntity taxaJurosEntity = new TaxaJurosEntity();
+		taxaJurosEntity.setAgencia("1234");
+		taxaJurosEntity.setConta("CC");
+		taxaJurosEntity.setSaldo(new BigDecimal(Math.floor(Math.random() * 99 - 1) + -999));
+		taxaJurosEntity.setLimite(new BigDecimal(13000));
+		taxaJurosEntity.setTaxaJuros(new BigDecimal(10));
+
+		return taxaDataproviderDomainMapper.getTaxaJurosCC(taxaJurosEntity);
+	}
+	
+	public TaxaJurosCCDomain getTaxaJurosCC20() {
+
+		TaxaJurosEntity taxaJurosEntity = new TaxaJurosEntity();
+		taxaJurosEntity.setAgencia("1234");
+		taxaJurosEntity.setConta("CC");
+		taxaJurosEntity.setSaldo(new BigDecimal(Math.floor(Math.random() * 99 - 1) + -999));
+		taxaJurosEntity.setLimite(new BigDecimal(8000));
+		taxaJurosEntity.setTaxaJuros(new BigDecimal(20));
+
+		return taxaDataproviderDomainMapper.getTaxaJurosCC(taxaJurosEntity);
+	}
+
+	public TaxaJurosCPDomain getTaxaJurosCP5() {
+
+		TaxaJurosEntity taxaJurosEntity = new TaxaJurosEntity();
+		taxaJurosEntity.setAgencia("1234");
+		taxaJurosEntity.setConta("CP");
+		taxaJurosEntity.setSaldo(new BigDecimal(Math.floor(Math.random() * 99 - 1) + -999));
+		taxaJurosEntity.setLimite(new BigDecimal(12000));
+		taxaJurosEntity.setTaxaJuros(new BigDecimal(5));
+
+		return taxaDataproviderDomainMapper.getTaxaJurosCP(taxaJurosEntity);
+	}
+	
+	public TaxaJurosCPDomain getTaxaJurosCP3() {
+
+		TaxaJurosEntity taxaJurosEntity = new TaxaJurosEntity();
+		taxaJurosEntity.setAgencia("1234");
+		taxaJurosEntity.setConta("CP");
+		taxaJurosEntity.setSaldo(new BigDecimal(Math.floor(Math.random() * 99 - 1) + -999));
+		taxaJurosEntity.setLimite(new BigDecimal(4000));
+		taxaJurosEntity.setTaxaJuros(new BigDecimal(3));
+
+		return taxaDataproviderDomainMapper.getTaxaJurosCP(taxaJurosEntity);
+	}
+	
+	public TaxaJurosCPDomain getTaxaJurosCP8() {
+
+		TaxaJurosEntity taxaJurosEntity = new TaxaJurosEntity();
+		taxaJurosEntity.setAgencia("1234");
+		taxaJurosEntity.setConta("CP");
+		taxaJurosEntity.setSaldo(new BigDecimal(Math.floor(Math.random() * 99 - 1) + -999));
+		taxaJurosEntity.setLimite(new BigDecimal(6700));
+		taxaJurosEntity.setTaxaJuros(new BigDecimal(8));
+
+		return taxaDataproviderDomainMapper.getTaxaJurosCP(taxaJurosEntity);
 	}
 }
---
+++ caixa-eletronico/caixa-eletronico/caixa-eletronico/src/main/java/br/com/ciandt/caixaeletronico/usecase/service/TaxaService.java
@@ -0,0 +1,36 @@
+package br.com.ciandt.caixaeletronico.usecase.service;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.ciandt.caixaeletronico.dataprovider.implementation.TaxaJurosImplementation;
+import br.com.ciandt.caixaeletronico.usecase.domain.TaxaDomain;
+
+@Service
+public class TaxaService {
+	
+	@Autowired
+	TaxaJurosImplementation taxaJurosImplementation;
+	
+	public TaxaDomain getTaxaJuros(BigDecimal taxaJuros) {
+		
+		if(taxaJuros.equals("CC") && taxaJuros.equals(new BigDecimal(1111))) {
+			return taxaJurosImplementation.getTaxaJurosCC15();
+		}else if(taxaJuros.equals("CC") && taxaJuros.equals(new BigDecimal(2222))) {
+			return taxaJurosImplementation.getTaxaJurosCC10();
+		}else if(taxaJuros.equals("CC") && !taxaJuros.equals(new BigDecimal(1111)) && !taxaJuros.equals(new BigDecimal(2222))) {
+			return taxaJurosImplementation.getTaxaJurosCC20();
+		}
+		
+		if(taxaJuros.equals("CP") && taxaJuros.equals(new BigDecimal(1111))) {
+			return taxaJurosImplementation.getTaxaJurosCP5();
+		}else if(taxaJuros.equals("CP") && taxaJuros.equals(new BigDecimal(2222))) {
+			return taxaJurosImplementation.getTaxaJurosCP3();
+		}else if(taxaJuros.equals("CP") && taxaJuros.equals(new BigDecimal(1111)) && taxaJuros.equals(new BigDecimal(2222))) {
+			return taxaJurosImplementation.getTaxaJurosCP8();
+		}
+		return null;
+	}
+}
